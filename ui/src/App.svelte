f592398c162a6860e4137a64265f128ec44f4984
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-07-09 13:35:54 +0300

    feat:change css lib

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,34 +1,16 @@
+<script>
+  import Counter from "./lib/Counter.svelte";
+
+</script>
 <main>
   <h1>Hi guys</h1>
   <div>
-    <button class="button">Default Button</button>
+    <button class="pure-button">Default Button</button>
   </div>
-  <ul>
-    <li>Unordered list item 1</li>
-    <li>Unordered list item 2</li>
-  </ul>
-  <div class="container">
-
-    <div class="row">
-      <div class="column">.column</div>
-      <div class="column">.column</div>
-      <div class="column">.column</div>
-      <div class="column">.column</div>
-    </div>
-  
-    <div class="row">
-      <div class="column">.column</div>
-      <div class="column column-50 column-offset-25">.column column-50 column-offset-25</div>
-    </div>
-  
-  </div>
-  <code>
-    <pre>
-      {`
-      .milligram {
-        color: #9b4dca;
-      }
-      `}
-    </pre>
-    </code>
+  <Counter />
+  <div class="pure-g">
+    <div class="pure-u-1 pure-u-md-1-3"> ... </div>
+    <div class="pure-u-1 pure-u-md-1-3"> ... </div>
+    <div class="pure-u-1 pure-u-md-1-3"> ... </div>
+</div>
 </main>
